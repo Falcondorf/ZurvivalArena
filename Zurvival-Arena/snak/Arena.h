0c3cd9d880e8c349a37c100692862b983d528a4d
Fakir <39717@heb.be>
2017-01-08 14:26:31 +0100

    Doc de arene.

--- Zurvival-Arena/snak/Arena.h
+++ Zurvival-Arena/snak/Arena.h
@@ -5,29 +5,73 @@
 #include <string>
 #include <mutex>
 
+/*!
+*\brief Class qui represente l'arene du jeu.
+*\file arena.h
+*\author Aynaou Dupont Fakir Bauwens
+*/
+
 class Arena
 {
 private:
 
-	unsigned width_;
-	unsigned height_;
-	std::vector<sf::RectangleShape> obstacles_;
-	std::vector<std::vector<bool>> tiles_;
+	unsigned width_;/*!< La longueur de l'arene.  */
+	unsigned height_;/*!< La largeur de l'arene  */
+	std::vector<sf::RectangleShape> obstacles_; /*!< Les obstacles presents dans l'arene tels que les maisons,puits...  */
+	std::vector<std::vector<bool>> tiles_;/*!< Matrice boolean qui represente les obstacles.  */
 
 public:
-	Arena()=default;
-	Arena(unsigned width,unsigned height);
+	/*!
+	* \brief Constructeur de Arena par defaut
+	*/
+	Arena() = default;
+	/*!
+	* \brief Constructeur de Arena avec parametre.
+	* \param width la longeur de l'arene
+	* \param height la largeur de l'arene
+	*/
+	Arena(unsigned width, unsigned height);
+	/*!
+	* \brief Permet de savoir si une case est accessible.
+	* \param x l'abcisse de la position a tester
+	* \param `y l'ordonnée de la position a vendre
+	* \return vrai si la case est accessible.
+	*/
 	bool isFree(float x, float y);
+	/*!
+	* \brief permet d'initialiser la matrice d'accessiblité.
+	*/
 	void setTiles();
+	/*!
+	* \brief Geteur de la longueur de l'arene.
+	* \return la longueur de l'arene.
+	*/
 	inline unsigned getWidth()const;
+	/*!
+	* \brief Geteur de la hauteur de l'arene.
+	* \return la honteur de l'arene.
+	*/
 	inline unsigned getHeight() const;
+	/*!
+	* \brief Geteur le nombre d'obstacle dans l'arene.
+	* \return le nombre d'obstacle dans l'arene.
+	*/
 	inline unsigned getNbObstacles() const;
+	/*!
+	* \brief Geteur d'un obstacle dans l'arene.
+	*\param index l'indicede l'obstacle qu'on veut.
+	* \return Un obstacle de present dans l'arene. 
+	*/
 	inline const  sf::RectangleShape & getObstacle(unsigned index) const;
+	/*!
+	* \brief Geteur de la matrice d'accessibilité  de l'arene.
+	* \return La matrice d'accessibilité de l'arene.
+	*/
 	inline const  std::vector<std::vector<bool>> & getTiles() const;
 };
 
 
-unsigned Arena::getNbObstacles()const 
+unsigned Arena::getNbObstacles()const
 {
 	return obstacles_.size();
 }
